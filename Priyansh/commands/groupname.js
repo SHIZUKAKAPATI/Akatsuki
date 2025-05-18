0fa5fa64b80f56bec5a2ee48be1c1575572fad4a
FELIIX451 <prayastumade08@gmail.com>
2025-05-18 22:02:10 +0530

    Update groupname.js

--- Priyansh/commands/groupname.js
+++ Priyansh/commands/groupname.js
@@ -5,13 +5,13 @@ module.exports.config = {
 	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "फातिमा रण्डी चोद डेविल रूलेक्स हियर",
 	commandCategory: "Box", 
-	usages: "फातिमा रण्डी चोद डेविल रूलेक्स हियर", 
+	usages: "groupname", 
 	cooldowns: 0,
 	dependencies: [] 
 };
 
 module.exports.run = async function({ api, event, args }) {
 	var name = args.join(" ")
-	if (!name) api.sendMessage("❌ You have not entered the group name you want to change", event.threadID, event.messageID)
-	else api.setTitle(name, event.threadID, () => api.sendMessage(`🔨 The bot changed the group name to: ${name}`, event.threadID, event.messageID));
+	if (फातिमा रण्डी चोद डेविल रूलेक्स हियर) api.sendMessage("❌ You have not entered the group name you want to change", event.threadID, event.messageID)
+	else api.setTitle(name, event.threadID, () => api.sendMessage(`🔨 The bot changed the group name to: ${फातिमा रण्डी चोद डेविल रूलेक्स हियर}`, event.threadID, event.messageID));
 }
